8018e7a7b495ad35e1526724b50a9f602189f209
corama <coramayunus@gmail.com>
2019-12-18 17:49:50 +0300

    more request handlers

--- routes/auth.js
+++ routes/auth.js
@@ -2,6 +2,17 @@ const express = require('express');
 const router = express.Router();
 const User = require('../model/User');
 
+// Get all the users
+router.get('/', async (req, res) => {
+    try {
+        const users = await User.find();
+        res.json(users);
+    } catch (error) {
+        res.json({message : error});
+    }
+})
+
+// Post and process all the users
 router.post('/register', async (req, res) => {
     const user = new User({
         name : req.body.name,
@@ -16,5 +27,37 @@ router.post('/register', async (req, res) => {
         res.status(400).send(error);
     }
 });
+
+// Get a specific user
+router.get('/:userID', async (req, res) => {
+    try {
+        const user = await User.findById(req.params.userID);
+        res.json(user)
+    } catch (error) {
+        res.json({message : error});
+    }
+})
+
+// Delete specific user
+router.delete('/:userID', async (req, res) => {
+    try {
+        const removedUser = await User.deleteOne({_id : req.params.userID});
+        res.json(removedUser);
+    } catch (error) {
+        res.json({message : error});
+    }
+})
+
+// Update a user 
+router.patch('/:UserID', async (req, res) =>{
+    try {
+        const updatedUser = await User.updateOne({_id : req.params.userID}, 
+            {$set : { name : req.body.name}});
+
+            res.json(updatedUser);
+    } catch (error) {
+        res.json({message : error});   
+    }
+})
 
 module.exports = router;
